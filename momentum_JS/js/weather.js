d18fd4f00980790bc111c1a481db95a3685942f4
richeeee128 <qoqw808@gmail.com>
2023-07-07 16:32:44 +0900

    feat : 기능 구현 완료

--- momentum_JS/js/weather.js
+++ momentum_JS/js/weather.js
@@ -1,11 +1,20 @@
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  console.log('You live it', lat, lng);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}8&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector('#weather span:first-child');
+      const weather = document.querySelector('#weather span:last-child');
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
+// fetch is promise 프로미스는 당장 뭔가 일어나지 않고 시간이 걸린 뒤에 일어남
 
 function onGeoError() {
   alert("can't find you. No weather for you.");
 }
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
-// todo weathermap API 기다리기
