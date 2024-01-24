dbce515e6e4a395638bf38b0fd6d8240f4194062
richeeee128 <qoqw808@gmail.com>
2024-01-24 17:14:47 +0900

    nwitter prac commit

--- nwitter-reloaded/src/App.tsx
+++ nwitter-reloaded/src/App.tsx
@@ -1,7 +1,31 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./routes/Home";
+import Layout from "./components/Layout";
+import Profile from "./routes/Profile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  <>
+  return (
+    <>
+      <RouterProvider router={router} />
     </>
+  );
 }
 
-export default App
+export default App;
