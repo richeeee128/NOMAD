c75861d3fe48e636edc3f24db0b34c051023f689
richeeee128 <qoqw808@gmail.com>
2024-08-14 22:31:05 +0900

    TS 3.4

--- TypeScript_Prac/Polymorphism.ts
+++ TypeScript_Prac/Polymorphism.ts
@@ -21,3 +21,35 @@ const b = superPrint([1, 2, 3, 4, 5, 6]);
 const c = superPrint([true, false, true]);
 const d = superPrint([1, 2, true, false]);
 const e = superPrint([1, 2, true, false, "hello"]);
+
+// 제네릭을 이용하여 타입을 생성하고 저장할 수도 있음
+// 타입의 재사용성이 엄청나다
+
+type Player<E> = {
+  name: string;
+  extraInfo: E;
+};
+
+type NicoExtra = {
+  favFood: string;
+};
+
+type NicoPlayer = Player<NicoExtra>;
+
+const nico: NicoPlayer = {
+  name: "nico",
+  extraInfo: {
+    favFood: "kimchi",
+  },
+};
+
+type arrNumbers = Array<number>;
+
+let one: arrNumbers = [1, 2, 3, 4];
+
+function printAllnumbers(arr: Array<number>) {
+  console.log(arr);
+}
+// function printAllnumbers(arr:number[]) {
+//   console.log(arr)
+// } 같은 예시. 제네릭을 정말 많이 씀. 당연함. 타입을 자동적으로 알게끔 해주므로써 타입 재사용성을 높여주기 때문에.
