cb0e5859dfe89bb94209de03320832a7aed76bfc
richeeee128 <qoqw808@gmail.com>
2024-08-22 14:48:24 +0900

    TS 5.4 JSDoc

--- TypeScript_Prac/TSChain/src/index.ts
+++ TypeScript_Prac/TSChain/src/index.ts
@@ -4,3 +4,6 @@ class Block {
     return "hi";
   }
 }
+import { init, exit } from "./myPackage";
+/**  tsconfig 파일에 allowJs : true로 변경해두면, JS에서 사용한 모든 함수들을 무리없이 사용할 수 있다. 다만 타입이 엉망이 될뿐
+ 해결방법이 존재한다. JSDoc으로! ts-check 이후 함수 위에 코멘트를 작성해두면 해결됨. */
