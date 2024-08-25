b24e96719f88ffff8d5d6fc610ec5230479a8907
richeeee128 <qoqw808@gmail.com>
2024-08-25 15:10:45 +0900

    Zoom 0.4 환경세팅 완료

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -2,11 +2,11 @@ import express, { application } from "express";
 import path from "path";
 
 const app = express();
-const dirname = path.resolve();
+const _dirname = path.resolve();
 
 app.set("view engine", "pug");
-app.set("views", dirname + "/src/views");
-
+app.set("views", _dirname + "/src/views");
+app.use("/public", express.static(path.join(_dirname, "src", "/public")));
 app.get("/", (req, res) => res.render("home"));
 
 //포트 3000을 의미함
